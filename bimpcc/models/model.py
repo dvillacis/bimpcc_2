fix: adjust initial t parameter and update Jacobian matrix calculations in MPCC and TVDenoising models

bimpcc/models/model.py
@@ -20,7 +20,7 @@ class MPCCModel(ABC):
         complementarity_constraint_func: ComplementarityConstraintFn,
         bounds: List[Tuple[Union[int, None], Union[int, None]]],
         x0: np.ndarray,
-        t_init=1e-2,
+        t_init=1.0,
         *args,
         **kwargs,
     ):

bimpcc/models/tvdenoising_model.py
@@ -357,7 +357,7 @@ class TVDenComplementarityConstraintFn(ComplementarityConstraintFn):
 
     def jacobian(self, x: np.ndarray) -> float:
         u, q, r, delta, theta, alpha = self.parse_vars(x)
-        Jalpha = sp.coo_matrix((np.ones(self.R), (np.arange(self.R), [0] * self.R)))
+        Jalpha = sp.coo_matrix((r*np.ones(self.R), (np.arange(self.R), [0] * self.R)))
         Jr = sp.coo_matrix((alpha - delta, (np.arange(self.R), np.arange(self.R))))
         Jdelta = sp.coo_matrix((r, (np.arange(self.R), np.arange(self.R))))
         # Jalpha = sp.coo_matrix((r, (np.arange(self.R), np.arange(self.R))))