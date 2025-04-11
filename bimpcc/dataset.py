fix: gradients in reconst regularized

bimpcc/dataset.py
@@ -11,19 +11,10 @@ def load_and_scale_image(image_path, target_pixels, add_noise=False, add_blur=Fa
     grayscale_image = np.array(resized_image) / np.max(resized_image)
     
     if add_noise:
-        # np.random.seed(0)
-        # noise = 0.05*np.random.randn(target_pixels, target_pixels)
-        # noisy_image = grayscale_image + noise
-        # noisy_image = np.clip(noisy_image, 0, 1)
-        # grayscale_image += noise
-        mean = 0
-        std = 0.2
         np.random.seed(0)
         noise = 0.05*np.random.randn(target_pixels, target_pixels)
-        noisy_image = grayscale_image + noise
-        noisy_image = np.clip(noisy_image, 0, 1)
-        grayscale_image = noisy_image
-
+        grayscale_image += noise
+        
     if add_blur:
         # kernel = np.array([[1, 2, 1], [2, 4, 2], [1, 2, 1]]) / 16
         # blurred_image = convolve2d(grayscale_image, kernel, mode='same', boundary='symm')
@@ -31,8 +22,10 @@ def load_and_scale_image(image_path, target_pixels, add_noise=False, add_blur=Fa
             grayscale_image.shape, kernel_size=3, sigma=1.0
         )
         blurred_image = blur_matrix @ grayscale_image.flatten()
+        # blurred_image = np.clip(blurred_image, 0, 1)
         grayscale_image = blurred_image.reshape(grayscale_image.shape)
 
+    grayscale_image = np.clip(grayscale_image, 0, 1)
     # Return the grayscale image
     return grayscale_image
 

bimpcc/models/recons_regularized.py
@@ -46,7 +46,7 @@ class TVReconsRegObjectiveFn(ObjectiveFn):
         u, q, alpha = self.parse_vars(x)
         residual = self.forward_map @ u - self.true_img
         return np.concatenate(
-            (self.forward_map @ residual, np.zeros(self.M + self.parameter_size))
+            (self.forward_map.T @ residual, np.zeros(self.M + self.parameter_size))
         )
 
     def hessian(self, x: np.ndarray) -> float:
@@ -85,7 +85,7 @@ class ReconsStateConstraintFn(ConstraintFn):
     def __call__(self, x: np.ndarray) -> float:
         u, q, alpha = self.parse_vars(x)
         residual = self.forward_map @ u - self.noisy_img
-        return self.forward_map @ residual + self.gradient_op.T @ q
+        return self.forward_map.T @ residual + self.gradient_op.T @ q
 
     def parse_vars(self, x):
         return _parse_vars(x, self.N, self.M)